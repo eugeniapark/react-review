391d4f7b0d908ff585824ffd9f2a78f88e800215
Eugenia Park <Eugenialpark@gmail.com>
2019-01-28 17:42:32 -0800

    new Component folder, App.jsx -- rendered from Component App.jsx

---
+++ client/component/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ReactDOM from 'react-DOM';
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <
+//   }
+// }
+
+var App = () => <div>Hello from Component!</div>;
+
+export default App;
